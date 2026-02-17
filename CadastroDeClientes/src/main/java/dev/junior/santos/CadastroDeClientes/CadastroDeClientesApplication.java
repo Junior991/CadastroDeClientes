80e08911c70ffc564a836a3d6c872648ebe7c3ab
Junior991 <juniorfernandes991@gmail.com>
2026-02-16 21:33:32 -0300

    Primeiro commit

---
+++ CadastroDeClientes/src/main/java/dev/junior/santos/CadastroDeClientes/CadastroDeClientesApplication.java
@@ -0,0 +1,14 @@
+package dev.junior.santos.CadastroDeClientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeClientesApplication.class, args);
+	}
+
+
+}
